4aba304bac23e6156c3d8e4a11423388c05ef7e5
Pruthviraj Chavan <pruthvirajchavan6877@gmail.com>
2025-12-07 16:09:50 +0530

    data operation on json

--- main.py
+++ main.py
@@ -5,6 +5,23 @@ from pathlib import Path
 from datetime import datetime
 
 class Library: 
+    database = "library.json"
+    data = {"books": [], "members": []}
+
+    # Load existing data to json or create your json
+
+    if Path(database).exists(): 
+        with open(database, "r") as fs: 
+            content = fs.read().strip()
+            if content: 
+                data = json.loads(content)
+            else: 
+                with open(database, 'w') as fs: 
+                    json.dump(data,fs,indent=4)
+
+
+
+
 
     def gen_id(Prefix = "B"): 
         random_id = ""
