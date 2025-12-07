6989df67e3d6aba73edb93a2721ec35ebc59b158
Pruthviraj Chavan <pruthvirajchavan6877@gmail.com>
2025-12-07 10:42:59 +0530

    display information

--- main.py
+++ main.py
@@ -4,3 +4,14 @@ import string
 from pathlib import Path
 from datetime import datetime
 
+print("="*50)
+print("Library Management System")
+print("="*50)
+print("1. Add Book")
+print("2. List Books")
+print("3. Add Members")
+print("4. List members")
+print("5. Borrow Book")
+print("6. Return Book")
+print("0. Exit the portal")
+print("-"*50)
