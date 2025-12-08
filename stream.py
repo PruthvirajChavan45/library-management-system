0fcd0d2cd559b09b3c42c417aecec8ea02b21718
Pruthviraj Chavan <pruthvirajchavan6877@gmail.com>
2025-12-08 10:07:17 +0530

    remove center-align

--- stream.py
+++ stream.py
@@ -20,16 +20,6 @@ st.set_page_config(
     layout="wide"
 )
 
-# Center text inside tables & metrics
-st.markdown("""
-<style>
-thead tr th:first-child {text-align: center !important;}
-tbody td {text-align: center !important;}
-thead th {text-align: center !important;}
-.stMetric { text-align: center !important; }
-</style>
-""", unsafe_allow_html=True)
-
 
 DATABASE_PATH = Path("library.json")
 
